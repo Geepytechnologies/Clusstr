cf09a0cf972e4b38cb8343e47449a57ecbd698c6
geepytechnologies <geepytechnologies@gmail.com>
2024-03-21 10:14:49 +0100

    removed text

--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -4,10 +4,7 @@ type Props = {};
 
 const Landingpage = (props: Props) => {
   return (
-    <div className="bg-[url('/img1.jpg')] min-h-screen w-full bg-center bg-cover">
-      Landingpag
-      <p className="text-red-500">hello</p>
-    </div>
+    <div className="bg-[url('/img1.jpg')] min-h-screen w-full bg-center bg-cover"></div>
   );
 };
 
